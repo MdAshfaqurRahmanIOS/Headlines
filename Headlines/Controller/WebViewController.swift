6ef5a54f4d5d3137453f9e65a57ff462087e9f52
Fahim Expo <fahimexpo32@gmail.com>
2020-11-23 13:42:37 +0600

    Custom menu implementation  ...completed

--- Headlines/Controller/WebViewController.swift
+++ Headlines/Controller/WebViewController.swift
@@ -11,9 +11,24 @@ import WebKit
 class WebViewController: UIViewController, WKUIDelegate {
     
     var weburl = ""
+    var webView: WKWebView!
 
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let myURL = URL(string: weburl)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
     }
 
 }
+
